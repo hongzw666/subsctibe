3d2b8095fb6313e6a5d54704f3e64832f60eb3a3
pc <pc@DESKTOP-6AQEV2O>
2019-04-21 15:45:40 +0800

    xiuy

--- src/main/java/com/hong/weixin/SubscribeApplication.java
+++ src/main/java/com/hong/weixin/SubscribeApplication.java
@@ -36,29 +36,15 @@ public class SubscribeApplication implements //
 		DisposableBean, //
 		// 得到Spring的容器
 		ApplicationContextAware {
+	private ApplicationContext ctx;// Spring容器
 
-	// 相当于Spring的XML配置方式中的<bean>元素
-	@Bean
-	public RedisTemplate<String, InMessage> inMessageTemplate(//
-			@Autowired RedisConnectionFactory redisConnectionFactory) {
-		RedisTemplate<String, InMessage> template = new RedisTemplate<>();
-		template.setConnectionFactory(redisConnectionFactory);
-		// 设置序列化程序
-//		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new JsonRedisSerializer());
-
-		return template;
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		ctx = applicationContext;
 	}
 
 	// 这是一个停止监视器，等待是否停止的通知
 	private final Object stopMonitor = new Object();
-	private ApplicationContext ctx;
-	
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		ctx = applicationContext;
-
-	}
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -82,46 +68,69 @@ public class SubscribeApplication implements //
 		}
 	}
 
+	// 相当于Spring的XML配置方式中的<bean>元素
+	@Bean
+	public RedisTemplate<String, InMessage> inMessageTemplate(//
+			@Autowired RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, InMessage> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+
+		// 设置一个序列化程序，就可以非常方便自动序列化！
+		// Redis是键值对方式存储数据的，所以其实KeySerializer是把键序列化成可以传输的数据。
+		// 由于泛型的时候已经确定，Key其实是String，所以可以使用系统默认的
+//		template.setKeySerializer(new StringRedisSerializer());
+
+		// 由于不确定是哪个类型，InMessage只是一个父类，它有许多不同的子类。
+		// 因此扩展Jackson2JsonRedisSerializer变得极其重要：重写方法、不要构造参数
+		template.setValueSerializer(new JsonRedisSerializer());
+//		template.setDefaultSerializer(new JsonRedisSerializer());
+
+		return template;
+	}
+
 	@Bean
 	public MessageListenerAdapter messageListener(@Autowired RedisTemplate<String, InMessage> inMessageTemplate) {
-
 		MessageListenerAdapter adapter = new MessageListenerAdapter();
 		// 共用模板里面的序列化程序
 		adapter.setSerializer(inMessageTemplate.getValueSerializer());
+
 		// 设置消息处理程序的代理对象
 		adapter.setDelegate(this);
 		// 设置代理对象里面哪个方法用于处理消息，设置方法名
 		adapter.setDefaultListenerMethod("handle");
-		return adapter;
 
+		return adapter;
 	}
 
 	@Bean
-	public RedisMessageListenerContainer messageListenerContainer(
-			@Autowired RedisConnectionFactory redisConnectionFactory, @Autowired MessageListener l) {
+	public RedisMessageListenerContainer messageListenerContainer(//
+			@Autowired RedisConnectionFactory redisConnectionFactory, //
+			@Autowired MessageListener l) {
 
 		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
 		container.setConnectionFactory(redisConnectionFactory);
+
 		// 给容器增加监听器
-		MessageListener messageListener = new MessageListener() {
+//		MessageListener l = new MessageListener() {
+//
+//			@Override
+//			public void onMessage(Message message, byte[] pattern) {
+//
+//			}
+//		};
 
-			@Override
-			public void onMessage(Message message, byte[] pattern) {
-				// TODO Auto-generated method stub
-
-			}
-		};
-
+		// 可以监听多个通道的消息
 		List<Topic> topics = new ArrayList<>();
+
 		// 支持*通配符，监听多个通道
-		// topic.add(new ChannelTopic("hongzw_*"));
+//		topics.add(new PatternTopic("kemao_1_*"));
 		// 监听具体某个通道
-		topics.add(new ChannelTopic("hongzw_event"));
+		topics.add(new ChannelTopic("kemao_1_event"));
 		container.addMessageListener(l, topics);
 
 		return container;
 	}
-	
+
 	private static final Logger LOG = LoggerFactory.getLogger(SubscribeApplication.class);
 
 	public void handle(EventInMessage msg) {
@@ -142,17 +151,18 @@ public class SubscribeApplication implements //
 			LOG.trace(e.getMessage(), e);
 		}
 	}
+
 	@Bean
 	public ObjectMapper objectMapper() {
-		
 		return new ObjectMapper();
 	}
 
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(SubscribeApplication.class, args);
 //		System.out.println("Spring Boot应用启动成功");
-//		// 要让程序等待 不要退出
+		// 让程序进入等待、不要退出
 //		CountDownLatch countDownLatch = new CountDownLatch(1);
 //		countDownLatch.await();
 	}
+
 }
