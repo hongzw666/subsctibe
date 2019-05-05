9d89f3cd48f276d94b2f900103d04a810ad03288
pc <pc@DESKTOP-6AQEV2O>
2019-05-05 22:40:08 +0800

    adds

--- src/main/java/com/hong/weixin/SubscribeApplication.java
+++ src/main/java/com/hong/weixin/SubscribeApplication.java
@@ -2,6 +2,7 @@ package com.hong.weixin;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -20,6 +21,7 @@ import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.Topic;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hong.domain.InMessage;
 import com.hong.domain.event.EventInMessage;
@@ -28,12 +30,12 @@ import com.hong.weixin.service.JsonRedisSerializer;
 
 @SpringBootApplication
 public class SubscribeApplication implements //
-		// 表示命令行执行的程序，要求实现一个run方法，在run方法里面启动一个线程等待停止通知
+			// 表示命令行执行的程序，要求实现一个run方法，在run方法里面启动一个线程等待停止通知
 		CommandLineRunner, //
-		// 当mvn spring-boot:stop命令执行以后，会发送一个停止的命令给Spring容器。
-		// Spring容器在收到此命令以后，会执行停止，于是在停止之前会调用DisposableBean里面的方法。
+			// 当mvn spring-boot:stop命令执行以后，会发送一个停止的命令给Spring容器。
+			// Spring容器在收到此命令以后，会执行停止，于是在停止之前会调用DisposableBean里面的方法。
 		DisposableBean, //
-		// 得到Spring的容器
+			// 得到Spring的容器
 		ApplicationContextAware {
 	private ApplicationContext ctx;// Spring容器
 
@@ -42,7 +44,7 @@ public class SubscribeApplication implements //
 		ctx = applicationContext;
 	}
 
-	// 这是一个停止监视器，等待是否停止的通知
+			// 这是一个停止监视器，等待是否停止的通知
 	private final Object stopMonitor = new Object();
 
 	@Override
@@ -67,22 +69,22 @@ public class SubscribeApplication implements //
 		}
 	}
 
-	// 相当于Spring的XML配置方式中的<bean>元素
+			// 相当于Spring的XML配置方式中的<bean>元素
 	@Bean
 	public RedisTemplate<String, InMessage> inMessageTemplate(//
 			@Autowired RedisConnectionFactory redisConnectionFactory) {
 		RedisTemplate<String, InMessage> template = new RedisTemplate<>();
 		template.setConnectionFactory(redisConnectionFactory);
 
-		// 设置一个序列化程序，就可以非常方便自动序列化！
-		// Redis是键值对方式存储数据的，所以其实KeySerializer是把键序列化成可以传输的数据。
-		// 由于泛型的时候已经确定，Key其实是String，所以可以使用系统默认的
-//		template.setKeySerializer(new StringRedisSerializer());
-
-		// 由于不确定是哪个类型，InMessage只是一个父类，它有许多不同的子类。
-		// 因此扩展Jackson2JsonRedisSerializer变得极其重要：重写方法、不要构造参数
+			// 设置一个序列化程序，就可以非常方便自动序列化！
+			// Redis是键值对方式存储数据的，所以其实KeySerializer是把键序列化成可以传输的数据。
+			// 由于泛型的时候已经确定，Key其实是String，所以可以使用系统默认的
+			//template.setKeySerializer(new StringRedisSerializer());
+			
+			// 由于不确定是哪个类型，InMessage只是一个父类，它有许多不同的子类。
+			// 因此扩展Jackson2JsonRedisSerializer变得极其重要：重写方法、不要构造参数
 		template.setValueSerializer(new JsonRedisSerializer());
-//		template.setDefaultSerializer(new JsonRedisSerializer());
+			//template.setDefaultSerializer(new JsonRedisSerializer());
 
 		return template;
 	}
@@ -95,7 +97,7 @@ public class SubscribeApplication implements //
 
 		// 设置消息处理程序的代理对象
 		adapter.setDelegate(this);
-		// 设置代理对象里面哪个方法用于处理消息，设置方法名
+			// 设置代理对象里面哪个方法用于处理消息，设置方法名
 		adapter.setDefaultListenerMethod("handle");
 
 		return adapter;
@@ -109,20 +111,20 @@ public class SubscribeApplication implements //
 		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
 		container.setConnectionFactory(redisConnectionFactory);
 
-		// 给容器增加监听器
-//		MessageListener l = new MessageListener() {
-//
-//			@Override
-//			public void onMessage(Message message, byte[] pattern) {
-//
-//			}
-//		};
-
-		// 可以监听多个通道的消息
+			// 给容器增加监听器
+			//MessageListener l = new MessageListener() {
+			//
+			//	@Override
+			//	public void onMessage(Message message, byte[] pattern) {
+			//
+			//	}
+			//};
+			
+			// 可以监听多个通道的消息
 		List<Topic> topics = new ArrayList<>();
 
 		// 支持*通配符，监听多个通道
-//		topics.add(new PatternTopic("kemao_1_*"));
+		//topics.add(new PatternTopic("hongzw_*"));
 		// 监听具体某个通道
 		topics.add(new ChannelTopic("hongzw_event"));
 		container.addMessageListener(l, topics);
@@ -133,10 +135,10 @@ public class SubscribeApplication implements //
 	private static final Logger LOG = LoggerFactory.getLogger(SubscribeApplication.class);
 
 	public void handle(EventInMessage msg) {
-		// 1.当前类实现ApplicationContextAware接口，用于获得Spring容器
-		// 2.把Event全部转换为小写，并且拼接上MessageProcessor作为ID
+			// 1.当前类实现ApplicationContextAware接口，用于获得Spring容器
+			// 2.把Event全部转换为小写，并且拼接上MessageProcessor作为ID
 		String id = msg.getEvent().toLowerCase() + "MessageProcessor";
-		// 3.使用ID到Spring容器获取一个Bean
+			// 3.使用ID到Spring容器获取一个Bean
 		try {
 			EventMessageProcessor mp = (EventMessageProcessor) ctx.getBean(id);
 			// 4.强制类型转换以后，调用onMessage方法
@@ -148,9 +150,9 @@ public class SubscribeApplication implements //
 		} catch (Exception e) {
 			LOG.warn("Bean的ID {} 无法调用对应的消息处理器: {}", id, e.getMessage());
 			LOG.trace(e.getMessage(), e);
-		}
+		} 
 	}
-
+	
 	@Bean
 	public ObjectMapper objectMapper() {
 		return new ObjectMapper();
@@ -158,10 +160,10 @@ public class SubscribeApplication implements //
 
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(SubscribeApplication.class, args);
-//		System.out.println("Spring Boot应用启动成功");
-		// 让程序进入等待、不要退出
-//		CountDownLatch countDownLatch = new CountDownLatch(1);
-//		countDownLatch.await();
+			//System.out.println("Spring Boot应用启动成功");
+			// 让程序进入等待、不要退出
+			//CountDownLatch countDownLatch = new CountDownLatch(1);
+			//countDownLatch.await();
 	}
 
 }
